c294819a7f2bc85151c852d8a7b3c62727f81267
mark mwendwa <mwendwamark310@gmail.com>
2023-07-18 11:36:55 +0300

    update the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../Styles/Home.css";
+import { Link } from "react-router-dom";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
@@ -28,51 +30,62 @@ function Home() {
             dolorem saepe eum. Nostrum eveniet ex unde dignissimos eos possimus
             ratione debitis.
           </p>
-          <button>
-            Get Started  -            
-            <FontAwesomeIcon icon={faArrowRight} />{" "}
-          </button>
+          <Link to="/login">
+            <button>
+              Get Started
+              <FontAwesomeIcon icon={faArrowRight} />{" "}
+            </button>
+          </Link>
         </header>
         <footer className="Home-footer">
           <div className="footer-content">
-            <p class>
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas perferendis a rem, consectetur odit animi autem temporibus consequatur! Ipsam voluptas minima in expedita voluptatibus magni iure aliquam, animi laborum corrupti aspernatur sit voluptatem odit?
+            {/* {" "}
+            <h2>Esmark Apartments</h2> */}
+            <p className="footer-content-p">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+              perferendis a rem, consectetur odit animi autem temporibus
+              consequatur! Ipsam voluptas minima in expedita voluptatibus magni
+              iure aliquam, animi laborum corrupti aspernatur sit voluptatem
+              odit?
             </p>
-            <div className="footer-links">
-              <p>Help</p>
-              <a href="discord">Location</a>
-              <br />
-              <a href="discord">Services</a>
-              <br />
-              <a href="discord">Pricing</a>
-              <br />
-              <a href="discord">About us</a>
-              <br />
-              <a href="discord">Terms and Contitions</a>
-            </div>
-            <div className="social-icons">
-              <a href="https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
-              <a href="https://www.facebook.com/login">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </a>
-              <a href="https://github.com/login">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-              </a>
-              <a href="">
-                <FontAwesomeIcon icon={faDiscord} size="2x" />
-              </a>
-              <a href="">
-                <FontAwesomeIcon icon={faTelegram} size="2x" />
-              </a>
-            </div>
-            {/* <p>&copy; 2023 Esmark Apartments | All rights reserved.</p>
+          </div>
+          <div className="help-footer-links">
+            <p>Help</p>
+            <a href="/location">Location</a>
+            <a href="/services">Services</a>
+            <a href="/pricing">Pricing</a>
+            <a href="/aboutus">About us</a>
+          </div>
+          <div className="support-footer-links">
+            <p>Support</p>
+            <a href="/privacy-policy">Privacy Policy</a>
+            <a href="/conditions">Terms and Contitions</a>
+            <a href="/houses">Houses</a>
+          </div>
+          <div className="social-icons">
+            <a href="https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://www.facebook.com/login">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a href="https://github.com/login">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="">
+              <FontAwesomeIcon icon={faDiscord} size="2x" />
+            </a>
+            <a href="">
+              <FontAwesomeIcon icon={faTelegram} size="2x" />
+            </a>
+          </div>
+          <div className="made-with-love">
+            <p>&copy; 2023 Esmark Apartments | All rights reserved.</p>
             <p>
               made wtih love{" "}
               <FontAwesomeIcon icon={faHeart} style={{ color: "#f01414" }} /> by
               Mark
-            </p> */}
+            </p>
           </div>
         </footer>
       </div>
